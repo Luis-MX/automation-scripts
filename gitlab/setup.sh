1db2235d7e73f0265fcc6f342969e24e8e1d5d48
Luis Enrique Ortiz Ramirez <luisortramirez2211@gmail.com>
2020-03-18 23:39:19 +0000

    Added script for Gitlab setup

---
+++ gitlab/setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install -y curl openssh-server ca-certificates
+sudo apt-get install -y postfix
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+echo 'Escribe la direccion en que se accedera a GitLab'
+read url
+sudo EXTERNAL_URL="http://35.184.186.109" apt-get install gitlab-ee
+
