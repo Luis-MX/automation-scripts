0d031c7ab61578e285213a49f5b2ad7ee2984a4b
Luis Enrique Ortiz Ramirez <luisortramirez2211@gmail.com>
2020-05-01 07:45:49 +0000

    Agregado lectura de direccion del servidor de gitlab

--- gitlab/setup.sh
+++ gitlab/setup.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install -y curl openssh-server ca-certificates
 sudo apt-get install -y postfix
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
-echo 'Escribe la direccion en que se accedera a GitLab'
+echo -n 'Escribe la direccion en que se accedera a GitLab:'
 read url
-sudo EXTERNAL_URL="http://35.184.186.109" apt-get install gitlab-ee
+sudo EXTERNAL_URL="http://$url" apt-get install gitlab-ee
 
